9025630c7000f0f695c623474c5b6044745aff54
wangaxuer <709372314@qq.com>
2024-12-11 21:38:53 +0800

    Add files via upload

---
+++ RCS.R
@@ -0,0 +1,43 @@
+library(ggplot2)
+library(rms)
+library(survminer)
+library(survival)
+EC<-read_excel("D:\\HuaweiMoveData\\Users\\86180\\Desktop\\.xlsx")
+head(EC)
+dd <- datadist(EC)
+options(datadist='dd')
+for(knot in 3:10)
+  fit <- cph(Surv(time,cancer)~rcs(CRP,5) + sex + age + race + BMI + 吸烟和不吸烟 + drink + MET + TDI + 癌症家族史 + income + 少睡失眠, X=TRUE,y=TRUE,data=EC)
+             tmp<- extractAIC(fit)
+             if(knot==5){AIC=tmp[2];nk=5}
+            
+fit <-cph(Surv(time,cancer)~rcs(CRP,5)+ sex + age + race + BMI + 吸烟和不吸烟 + drink + MET + TDI + 癌症家族史 + income + 少睡失眠,X=TRUE,y=TRUE,data=EC)
+
+anova(fit)
+
+# 获取卡方统计量和自由度
+chi_sq <- 569.36
+df <- 15
+
+# 使用卡方分布计算P值
+p_value <- pchisq(chi_sq, df, lower.tail = FALSE)
+
+# 打印精确的P值
+print(p_value, digits = 10)
+
+
+HR<-Predict(fit,C反应蛋白,fun=exp)
+head(HR)
+write.csv(HR, file = "D:\\HuaweiMoveData\\Users\\86180\\Desktop\\RCS CRP incidence.csv", row.names = FALSE)
+
+ggplot()+
+  geom_line(data=HR,aes(WBC,yhat),
+            linetype="solid",size=2,alpha = 10.0,colour="#a7b9d7")+
+        geom_ribbon(data=HR,
+                   aes(WBC,ymin =lower,ymax=upper),
+                   alpha = 0.4,fill="#a7b9d7")+
+  theme_classic()+
+  geom_hline(yintercept=1,linetype=2,size=1)+
+  labs(title ="for lung cancer Mortality",x="WBC",y="HR(95%CI)")
+
+
